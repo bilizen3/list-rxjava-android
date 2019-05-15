bc211f6ab24e204608eddad2d05cac8802ca23d9
Bill Flores <billflores@Bills-iMac.local>
2019-05-15 17:58:30 -0500

    add observable

--- app/src/main/java/com/flores/listrxjavaandroid/MainActivity.kt
+++ app/src/main/java/com/flores/listrxjavaandroid/MainActivity.kt
@@ -2,11 +2,21 @@ package com.flores.listrxjavaandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.rxkotlin.toObservable
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list = listOf("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
+
+        list.toObservable().filter { it.length > 5 }.subscribeBy(  // named arguments for lambda Subscribers
+            onNext = { println(it) },
+            onError = { it.printStackTrace() },
+            onComplete = { println("Done!") }
+        )
     }
 }
