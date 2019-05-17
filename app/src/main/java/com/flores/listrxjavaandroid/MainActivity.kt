ec2db27394b50bdb3ea66c9e7ae72f92df6af1fe
Bill Flores <billflores@Bills-iMac.local>
2019-05-17 18:55:47 -0500

    demo

--- app/src/main/java/com/flores/listrxjavaandroid/MainActivity.kt
+++ app/src/main/java/com/flores/listrxjavaandroid/MainActivity.kt
@@ -2,9 +2,14 @@ package com.flores.listrxjavaandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.reactivex.Observable
+import io.reactivex.ObservableOnSubscribe
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.rxkotlin.toObservable
 import kotlinx.android.synthetic.main.activity_main.*
+import org.reactivestreams.Subscriber
+import org.reactivestreams.Subscription
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,16 +20,48 @@ class MainActivity : AppCompatActivity() {
         val list = listOf("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
 
         var results = ""
+        var error = ""
+        var complete = ""
 
         list.toObservable().filter { it.length > 5 }.subscribeBy(  // named arguments for lambda Subscribers
             onNext = { results = "next" },
-            onError = { results = "error" },
-            onComplete = { results = "complete" }
+            onError = { error = "error" },
+            onComplete = { complete = "complete" }
         )
 
         btnShow.setOnClickListener {
-
+            Log.i("test", results)
+            Log.i("test", error)
+            Log.i("test", complete)
         }
+
+
+        val integerObservable = Observable.create(ObservableOnSubscribe<Any> {
+            it.onNext(1)
+            it.onNext(2)
+            it.onNext(3)
+            it.onComplete()
+        })
+
+        val integerSubscriber = object : Subscriber<Any> {
+            override fun onComplete() {
+                Log.i("test", "complete")
+            }
+
+            override fun onSubscribe(s: Subscription?) {
+                Log.i("test", "")
+            }
+
+            override fun onNext(t: Any?) {
+                Log.i("test", t as String)
+            }
+
+            override fun onError(t: Throwable?) {
+                Log.i("test", t.toString())
+            }
+        }
+
+        integerObservable.subscribe(integerSubscriber)
 
     }
 }
