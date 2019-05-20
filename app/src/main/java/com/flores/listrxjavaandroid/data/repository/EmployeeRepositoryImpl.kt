0d41f290d6575a31689cbe7ebfd9c886c7cedc91
Bill Flores <billflores@Bills-iMac.local>
2019-05-20 18:47:01 -0500

    demo

---
+++ app/src/main/java/com/flores/listrxjavaandroid/data/repository/EmployeeRepositoryImpl.kt
@@ -0,0 +1,9 @@
+package com.flores.listrxjavaandroid.data.repository
+
+import com.flores.listrxjavaandroid.data.model.Employee
+
+class EmployeeRepositoryImpl : EmployeeRepository {
+    override fun getEmployee(): List<Employee> {
+        return null
+    }
+}
